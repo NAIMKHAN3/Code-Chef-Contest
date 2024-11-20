1338bc7dfde601758e1b934fd9cdae019434c3f6
Naim Khan <naimkhan01856@gmail.com>
2024-11-20 20:38:01 +0600

    Problem 1 solved

---
+++ (161)-Week-4/Moneymaking.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x,y;
+    cin >> x >> y;
+    int X = x * 5000;
+    int Y = y * 9800;
+    cout << X+Y << endl;
+    return 0;
+}
