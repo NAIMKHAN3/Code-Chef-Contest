b318b316aa29a7764f1918ef3cc0b4287694844f
Naim Khan <naimkhan01856@gmail.com>
2024-12-18 20:36:19 +0600

    problem 1 solved

---
+++ (165)-Week-8/Christmas_Cake.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x;
+    cin >> x;
+    int ans = 25 - x;
+    cout << ans << endl;
+    return 0;
+}
