b61ff4e219fa86b66659f40321cc8a5e791c71ff
Naim Khan <naimkhan01856@gmail.com>
2025-02-19 20:53:41 +0600

    problem 1 solved

---
+++ (174)-Week-17/Too_Much_Homework.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x,y;
+    cin >> x >> y;
+    int ans = x + (10 * y);
+   if(ans >= 100){
+    cout << "Yes" << endl;
+   }else{
+    cout << "No" << endl;
+   }
+    return 0;
+}
